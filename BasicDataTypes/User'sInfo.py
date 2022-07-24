caf946dec40a888007f616faaaf1524761397a5d
Esra Koc <90002015+esrakocx@users.noreply.github.com>
2022-07-24 12:55:27 +0300

    Delete User'sInfo.py

--- BasicDataTypes/User'sInfo.py
+++
@@ -1,7 +0,0 @@
-name = input("Please enter your name: ")
-surname = input("Please enter your surname: ")
-number = input("Please enter your number: ")
-
-info=[name, surname, number]
-
-print("Your name is: {}\nYour surname is: {}\nYour number is: {}".format(info[0], info[1], info[2]))
