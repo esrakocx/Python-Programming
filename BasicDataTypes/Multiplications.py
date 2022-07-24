0aec8dade8360c2d0e81f852fa1acede5303d2d6
Esra Koc <eocesra@gmail.com>
2022-07-24 12:17:37 +0300

    Multiplication of three numbers

---
+++ BasicDataTypes/Multiplications.py
@@ -0,0 +1,12 @@
+print("~Multiplication of 3 numbers~\n")
+
+print("Please enter the 1st number you wanna multiply:")
+number1 = int(input("1st number: "))
+print("Please enter the 2nd number you wanna multiply:")
+number2 = int(input("2nd number: "))
+print("Please enter the 1st number you wanna multiply:")
+number3 = int(input("3rd number: "))
+
+result = number1*number2*number3
+
+print("\nResult is: {}".format(result))
