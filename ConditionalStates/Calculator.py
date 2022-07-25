79533536b46c6856d2c5fde5109c4bd7ca3a0d14
Esra Koc <eocesra@gmail.com>
2022-07-25 12:38:12 +0300

    A basic calculator

---
+++ ConditionalStates/Calculator.py
@@ -0,0 +1,28 @@
+print(""" 
+***********************
+~Calculator~
+
+Operations:
+
++ -> Addition
+- -> Subtraction
+x -> Multiplication
+/ -> Division
+
+***********************
+""")
+
+number1=float(input("Please enter the first number you want to operate: "))
+number2=float(input("Please enter the second number you want to operate: "))
+operation=input("Please enter the operation you want to do:")
+
+if operation == "+":
+    print("{} + {} = {}".format(number1, number2, number1+number2))
+elif operation == "-":
+    print("{} - {} = {}".format(number1, number2, number1-number2))
+elif operation == "x":
+    print("{} x {} = {}".format(number1, number2, number1*number2))
+elif operation == "/":
+    print("{} / {} = {}".format(number1, number2, number1/number2))
+else:
+    print("You entered wrong operation, please try again!")
