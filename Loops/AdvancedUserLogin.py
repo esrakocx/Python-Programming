23e2209cab36850948f1b2482061cd04d1dc5401
Esra Koc <eocesra@gmail.com>
2022-07-28 00:05:19 +0300

    ATM Machine

--- Loops/AdvancedUserLogin.py
+++ Loops/AdvancedUserLogin.py
@@ -8,13 +8,16 @@ User Login Screen
 
 system_name = "user1"
 system_password = "12345"
-login_chance = 3
+login_chance = 2
 check = 282
 
 while True:
     name = input("Username: ")
     password = input("Password: ")
 
+    if login_chance == 0:
+        print("Your chance to login is over!")
+        break
     if system_name != name and system_password == password:
         print("Incorrect username!")
         login_chance -= 1
@@ -22,7 +25,7 @@ while True:
         print("Incorrect password!")
         response = input("Forgot password? Y/N ")
         login_chance -= 1
-        if response == "Y" or response == "y": 
+        if response == "Y" or response == "y":
             print("We sent a code to your account for password reset.")
             code = int(input("Code: "))
             if code == check:
@@ -33,17 +36,14 @@ while True:
                 print("Try Again! Check your code!")
         elif response == "N" or response == "n":
             print("Please try again then!")
-            break
+            continue
         else:
             print("Incorrect input!")
-            break
+            continue
     elif system_name != name and system_password != password:
         print("Incorrect password and user name!")
         login_chance -= 1
     else:
         print("Welcome!")
         login_chance -= 1
-        break
-    if login_chance == 0:
-        print("Your chance to login is over!")
         break
---
+++ Loops/AtmMachine.py
@@ -0,0 +1,40 @@
+print("Welcome to the ATM Machine")
+pin = "1223"
+chance = 2
+balance = 1000
+
+while True:
+    user_pin = input("Please enter PIN: ")
+    if chance == 0:
+        print("Your account was locked!")
+        break
+    if pin == user_pin:
+        chose = input("""Please choose what you want to do.
+        1. Remaining balance
+
+        2. Deposit
+
+        3. Withdraw money\nChose: """)
+
+        if chose == "1":
+            print("Your balance: {}".format(balance))
+        elif chose == "2":
+            deposit = int(input("Enter the amount you want to deposit: "))
+            balance += deposit
+            print("Your balance: {}".format(balance))
+        elif chose == "3":
+            withdraw = int(input("Enter the amount you want to withdraw: "))
+
+            if withdraw < balance:
+                balance -= withdraw
+                print("Your balance: {}".format(balance))
+                continue
+            else:
+                print("Insufficient balance")
+        else:
+            print("Invalid transaction")
+            continue
+    else:
+        print("Incorrect PIN")
+        chance -= 1
+        continue
