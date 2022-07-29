8d1ff2e0f9aa37e6d621100bfbd5684fcf58f521
Esra Koc <eocesra@gmail.com>
2022-07-29 23:50:16 +0300

    Finding the prime numbers!

---
+++ Functions/Primes.py
@@ -0,0 +1,26 @@
+print("~Primes~\n")
+
+
+def prime(number):
+    if number == 1:
+        return False
+    elif number == 2:
+        return True
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                return False
+        return True
+
+while True:
+    number = input("Number: ")
+
+    if number == "q":
+        break
+    else:
+        number = int(number)
+
+        if(prime(number)):
+            print(number, "is a prime number.")
+        else:
+            print(number, "is not a prime number.")
