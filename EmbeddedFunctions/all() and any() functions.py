78814b5e8633409e88c96aba208237f473710cff
Esra Koc <eocesra@gmail.com>
2022-08-24 01:59:09 +0300

    all() and any() functions

---
+++ EmbeddedFunctions/all() and any() functions.py
@@ -0,0 +1,7 @@
+# all() function returns True if all values are True. If at least value is False it returns False.
+# any() function returns False if all values are False. If at least value is True it returns True.
+
+list1 = [True, False, True, False, True]
+
+print(all(list1))
+print(any(list1))
