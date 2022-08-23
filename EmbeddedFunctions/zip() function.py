e2c004c4e0813a001dfd076c20f5e8ac5b46736f
Esra Koc <eocesra@gmail.com>
2022-08-24 01:33:53 +0300

    zip() function

---
+++ EmbeddedFunctions/zip() function.py
@@ -0,0 +1,5 @@
+names = ["Kerim", "Tarık", "Ezgi", "Kemal", "İlkay", "Şükran", "Merve"]
+surnames = ["Yılmaz", "Öztürk", "Dağdeviren", "Atatürk", "Dikmen", "Kaya", "Polat"]
+
+for i, j in zip(names, surnames):
+    print(i, j)
